344d3b8b4f3e64472d0915d9328ed0a0313d5d25
Sobolev515 <alex.sobolev515@yandex.ru>
2024-03-02 10:47:00 +0500

    Add files via upload

--- ball.py
+++ ball.py
@@ -9,8 +9,9 @@ canvas.pack()
 tk.update()
 
 class Ball:
-    def __init__(self, size, canvas, color):
+    def __init__(self, size, canvas, color, paddle):
         self.canvas = canvas
+        self.paddle = paddle
         self.id = canvas.create_oval(size, size, 25, 25, fill=color)
         self.canvas.move(self.id, 245, 100)
         self.speeds = [1, 2, 3]
@@ -18,14 +19,27 @@ class Ball:
         self.y = random.choice([1, -1]) * random.choice(self.speeds)
         self.canvas_height = self.canvas.winfo_height()
         self.canvas_width = self.canvas.winfo_width()
+        self.bottom_hit = False
 
     def draw(self):
         self.canvas.move(self.id, self.x, self.y)
         pos = self.canvas.coords(self.id)
+        if pos[3] >= self.canvas_height:
+            self.bottom_hit = True
         if pos[3] >= self.canvas_height or pos[1] <= 0:
             self.y = -self.y
         if pos[0] <= 0 or pos[2] >= self.canvas_width:
             self.x = -self.x
+        if self.hit_paddle(pos) == True:
+            self.y = -self.y
+
+    def hit_paddle(self, pos):
+        paddle_pos = self.canvas.coords(self.paddle.id)
+        if pos[2] >= paddle_pos[0] and pos[0] <=paddle_pos[2]:
+            if pos[3]>= paddle_pos[1] and pos[3] <= paddle_pos[3]:
+                return True
+        return False
+
 
 class Paddle:
     def __init__(self, canvas, color):
@@ -34,8 +48,8 @@ class Paddle:
         self.canvas.move(self.id, 200, 300)
         self.x = 0
         self.canvas_width = self.canvas.winfo_width()
-        self.canvas.bind_all('<KeyPress-Left>', self.turn_left())
-        self.canvas.bind_all('<KeyPress-Right>', self.turn_right())
+        self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+        self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
 
     def draw(self):
         self.canvas.move(self.id, self.x, 0)
@@ -50,11 +64,12 @@ class Paddle:
         self.x = 2
 
 paddle = Paddle(canvas, 'blue')
-ball = Ball(10, canvas, 'red')
+ball = Ball(10, canvas, 'red', paddle)
 
 while True:
-    ball.draw()
-    paddle.draw()
+    if ball.bottom_hit == False:
+        ball.draw()
+        paddle.draw()
     tk.update_idletasks()
     tk.update()
     time.sleep(0.01)
